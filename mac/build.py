e8aa8f7b674d7ee3e2b195cae355a341d0acf25a
kuchtak <k.kuchta@bioinfo.pl>
2019-02-05 14:49:20 +0100

    Fix builing mac app.

--- mac/build.py
+++ mac/build.py
@@ -1,23 +1,23 @@
 from setuptools import setup
 from hpxbuilder import utils
+import os
 
 def process():
     import sys
     sys.argv.append("py2app")
 
-    APP = [utils.get_hprox_exec_path()]
-    DATA_FILES = []
-    PKGS = []
     OPTIONS = {
-        'packages': PKGS,
+        'argv_emulation':True,
+        'packages': [],
         'resources': [
             utils.get_hprox_media_dir_path(),
-            utils.get_hprox_templates_dir_path()]
+            utils.get_hprox_templates_dir_path()],
+        'iconfile': os.path.join(utils.get_hprox_media_dir_path(), "images", "Desktop_icon.icns")
     }
 
     setup(
-        app=APP,
-        data_files=DATA_FILES,
+        app=[utils.get_hprox_exec_path()],
+        data_files=[],
         options={'py2app': OPTIONS},
         setup_requires=['py2app'],
     )
