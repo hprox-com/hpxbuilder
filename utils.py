feabfdfd51eddd3eead5411d5b9e218b4e6ba9b7
Damian Peszko <d.peszko@bioinfo.pl>
2019-02-22 15:19:28 +0100

    Added new utils

--- utils.py
+++ utils.py
@@ -9,6 +9,29 @@ def get_builder_base():
     return os.path.dirname(__file__)
 
 
+def get_dist_dir():
+    return os.path.dirname(get_builder_base())
+
+
+def get_hpxqt_version():
+    return hpxqt.__version__
+
+
+def get_compressed_name():
+    _os = platform.system().lower()
+    compressed_name_map = {
+        'darwin': 'hprox-{version}-osx',
+        'linux': 'hprox-{version}-x86_64-linux-gnu'
+    }
+    return compressed_name_map[_os].format(version=get_hpxqt_version())
+
+
+def get_exec_name():
+    if platform.system().lower() == 'windows':
+        return 'hprox-{version}-win64'.format(version=get_hpxqt_version())
+    return 'hprox'
+
+
 def get_hpqt_dir():
     return os.path.dirname(inspect.getfile(hpxqt))
 
