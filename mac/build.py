0ff424e3ce6e9ec898d0d9ffe1381ca8a37dc09a
Damian Peszko <d.peszko@bioinfo.pl>
2019-02-22 16:13:23 +0100

    Minor update

--- mac/build.py
+++ mac/build.py
@@ -6,11 +6,14 @@ from hpxbuilder import utils
 
 
 def compress_app():
-    app_dir = os.path.join(utils.get_dist_dir(), 'hprox.app')
-    compressed_fpath = os.path.join(utils.get_dist_dir(), '%s.zip' % utils.get_compressed_name())
+    app_dir = os.path.join(utils.get_dist_dir(),
+                           '%.app' % utils.get_exec_name())
+    compressed_fpath = os.path.join(utils.get_dist_dir(),
+                                    '%s.zip' % utils.get_compressed_name())
 
     with zipfile.ZipFile(compressed_fpath, 'w', zipfile.ZIP_DEFLATED) as zf:
         for root, dirs, files in os.walk(app_dir):
+            root = os.path.abspath(root)
             for f in files:
                 src_path = os.path.join(root, f)
                 arcname = os.path.join(src_path.split('dist/')[1])
